85e5f5df1e3fb9562f4435ef5c95fdb2841b443e
Jeevan Neupane <078bct036@student.ioepc.edu.np>
2023-03-16 20:56:12 +0545

    Basic Design is Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { Brand,Cta,Navbar, } from "./Components";
+import { Footer,Blog,Header, Possiblities, WhatGPT3,Features } from "./Pages";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possiblities/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
     </div>
-  );
+   
+   )
 }
 
 export default App;
